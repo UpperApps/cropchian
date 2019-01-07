8e94c2f3c6df91c3dcab2659ac2092a57d1d2851
Rodrigo Melo <rodrigo.melo@skipthedishes.ca>
2019-01-07 01:21:28 -0600

    Included JSDoc configuration.

---
+++ jsdoc-config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+    plugins: ['node_modules/jsdoc-vuejs', 'plugins/markdown'],
+    source: {
+        includePattern: '\\.(vue|js)$',
+        include: ['src', 'tests']
+    },
+    babel: {
+        extensions: ['js', 'es6', 'jsx'],
+        presets: ['es2015', 'es2015-loose', 'stage-1']
+    },
+    opts: {
+        template: 'node_modules/tui-jsdoc-template', // same as -t templates/default
+        encoding: 'utf8', // same as -e utf8
+        destination: './docs/', // same as -d ./out/
+        recurse: true, // same as -r
+        readme: 'README.md'
+    },
+    tags: {
+        allowUnknownTags: true,
+        dictionaries: ['jsdoc']
+    },
+    templates: {
+        name: 'Vue-Vuetify Seed Project Documentation',
+        logo: {
+            url:
+                'C:/Repositories/UpperApps/vue-vuetify-seed-project/public/img/icons/favicon-32x32.png',
+            width: '32px',
+            height: '32px',
+            link:
+                'C:/Repositories/UpperApps/vue-vuetify-seed-project/docs/index.html'
+        },
+        footerText: 'Code rocks!',
+        useCollapsibles: true,
+        tabNames: {
+            api: 'API',
+            tutorials: 'Examples'
+        },
+        cleverLinks: true,
+        monospaceLinks: true
+    },
+    default: {
+        outputSourceFiles: true,
+        includeDate: true
+    }
+};
